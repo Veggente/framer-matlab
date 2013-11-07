d29500eabf3722270150cde13a6c3ccf637fb11b
Veggente <veggente@gmail.com>
2013-11-06 17:45:16 -0700

    Created a folder for network six-grid

--- codes/main_max.m
+++ codes/main_max.m
@@ -11,30 +11,36 @@ maximal_schedule_matrix = [ % six grid
         1 0 0 0;
         0 0 1 1];
 maximal_arrival = frame_size; % maximum packet number
-seed = rng('shuffle'); % fix rng seed
-maximal_iteration_num = 3; % maximum iteration
-maximal_traffic_num = 5; % number of traffic patterns
-interference_degree = 2;
+seed = rng('shuffle'); % shuffle rng seed based on time
+maximal_iteration_num = 30; % maximum iteration
+maximal_traffic_num = 50; % number of traffic patterns
+interference_degree = 3;
+maximum_independent_set_size = 3;
+network = 'six-grid'; % network topology
 
 % read current ratio-speedup-diagram
 rsd = ones(1, interference_degree); % rsd initialized to an upper bound
-if ~(exist('rsd.txt', 'file') == 2)
-    dlmwrite('rsd.txt', rsd);
+data_directory = ['../rsd/', network, '/'];
+rsdfile = [data_directory, 'rsd.txt']; % rsd filename
+if ~(exist(rsdfile, 'file') == 2)
+    dlmwrite(rsdfile, rsd);
 else
-    rsd = dlmread('rsd.txt');
+    rsd = dlmread(rsdfile);
 end
 
-for speed_up = 1:interference_degree
+for speed_up = 1:min(interference_degree, maximum_independent_set_size-1)
     [worst_traffic, worst_max, best_max, ratio] =...
         ratio_speedup_diagram_for_maximal(maximal_schedule_matrix,...
         speed_up, frame_size, maximal_traffic_num, maximal_iteration_num);
     if ratio < rsd(speed_up)
         rsd(speed_up) = ratio;
-        save(['record', num2str(speed_up), '.mat'], 'worst_traffic', 'worst_max', 'best_max');
+        save([data_directory, 'record', num2str(speed_up), '.mat'],...
+            'worst_traffic', 'worst_max', 'best_max');
     end
 end
-dlmwrite('rsd.txt', rsd);
+dlmwrite(rsdfile, rsd);
 
+% inspect rsd
 figure;
 stem(rsd);
 grid on;
